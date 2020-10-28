d3540614ff1598e1287b5aa38ac7f01ec0b5e56e
= <owr5273@gmail.com>
2020-10-27 23:36:19 -0300

    workable script stuff but code is very bad yo

--- ddScript.c
+++ ddScript.c
@@ -2,11 +2,16 @@
 #include <ddcGArray.h>
 #include <ddcDef.h>
 #include <ddcArguments.h>
+#include <stdio.h>
+#include <ddcCursor.h>
+#include <time.h>
+#include <ddcTime.h>
 
 typedef enum dstypes dstypes;
+
 enum dstypes {
 	dtint=0,
-	dtstring=0
+	dtstring=1
 };
 
 typedef struct Stack Stack;
@@ -36,6 +41,7 @@ void stack_add(Stack* stack, ddString name, void* value, dstypes type)
 	stack->len++;
 }
 
+typedef struct FNStack FNStack;
 struct FNStack
 {
 	ddString* names;
@@ -70,51 +76,206 @@ void FNStack_call(FNStack fn, ddString fu)
 	}
 }
 
+void parse_comms(ddString line, ddString* comms)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		ddString_empty(&(comms[i]));
+	}
+	int comc = 0;
+	for (int i = 0; i < line.length; i++)
+	{
+		if (line.cstr[i] == ' ')
+		{
+			comc++;	
+			continue;
+		}
+		ddString_push_char_back(&(comms[comc]), line.cstr[i]);
+	}
+}
 
+int stack_get_index(const Stack stack, const ddString name)
+{
+	for (int i = 0; i < stack.len; i++)
+	{
+		if (ddString_compare(name, stack.names[i]))
+		{
+			return i;
+		}
+	}
+}
+ddString file[20];
+ddsize flen;
+ddString* comms;
+
+//FNStack_add(&fnstack, make_constant_ddString("print"), 
+
+void compute(void)
+{
+	Stack stack = make_stack(20);
+	FNStack fnstack = make_FNStack(10);
+	for (int i = 0; i < flen; i++)
+	{
+		parse_comms(file[i], comms);
+		if (ddString_compare_cstring(comms[0], "add"))
+		{
+			int indx = stack_get_index(stack, comms[1]);
+			switch (stack.typs[indx])
+			{
+				case dtint: 
+				{
+					int* v = ((int*)__ddGArray_get(stack.vals, indx));
+					int t = ddString_to_int(make_constant_ddString(comms[2].cstr));
+					(*v) += t;
+				}
+			}
+		}
+		if (ddString_compare_cstring(comms[0], "if"))
+		{// if a == 1 then
+			int indx = stack_get_index(stack, comms[1]);
+			int val = ddGArray_get(stack.vals, indx, int);
+			int comp = ddString_to_int(make_constant_ddString(comms[3].cstr));
+			if (val == comp)
+			{
+				if (ddString_compare_cstring(comms[5], "goto"))
+				{
+					int g = ddString_to_int(make_constant_ddString(comms[6].cstr));
+					i = g-1;
+					continue;
+				}
+				if (ddString_compare_cstring(comms[5], "end"))
+				{
+					return;
+				}
+
+			}
+		}
+		if (ddString_compare_cstring(comms[0], "goto"))
+		{
+			int g = ddString_to_int(make_constant_ddString(comms[1].cstr));
+			i = g-1;
+			continue;
+		}
+		if (ddString_compare_cstring(comms[0], "usleep"))
+		{
+			int g = ddString_to_int(make_constant_ddString(comms[1].cstr));
+			usleep(g);
+		}
+		if (ddString_compare_cstring(comms[0], "sleep"))
+		{
+			int g = ddString_to_int(make_constant_ddString(comms[1].cstr));
+			sleep(g);
+		}
+		if (ddString_compare_cstring(comms[0], "let"))
+		{
+			if (ddString_compare_cstring(comms[1], "int"))
+			{
+				int* v = make(int, 1);
+				(*v) = ddString_to_int(make_constant_ddString(comms[4].cstr));
+				stack_add(&stack, make_ddString((comms[2].cstr)),
+						(void*)v, dtint);
+			}
+			if (ddString_compare_cstring(comms[1], "str"))
+			{
+				ddString v = make_ddString(comms[4].cstr);
+				stack_add(&stack, make_ddString((comms[2].cstr)),
+						(void*)&v, dtstring);
+			}
+		}
+		if (ddString_compare_cstring(comms[0], "print"))
+		{
+			int indx = stack_get_index(stack, comms[1]) - 1;
+			switch (stack.typs[indx])
+			{
+				case dtint: ddPrint_int_nl(ddGArray_get(stack.vals, indx, int));
+				case dtstring: ddPrint_ddString(ddGArray_get(stack.vals, indx, ddString));
+			}
+		}
+	}
+}
 
 int main(ddsize agsc, char** ags)
 {
 	ddArgs da = init_ddArgs(ags, agsc);
 
+	comms = make(ddString, 22);
+	flen = 0;
 
-	ddString file[4] = {
-		make_constant_ddString("let int a = 5"),
-		make_constant_ddString("set a = a + 2"),
-		make_constant_ddString("set a = a * 2"),
-		make_constant_ddString("fn print a")
-	};
 
-	Stack stack = make_stack(20);
-	FNStack fnstack = make_FNStack(10);
-	//FNStack_add(&fnstack, make_constant_ddString("print"), 
-	
-
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 20; i++)
 	{
-		if (file[i].cstr[0] == 'l' &&
-			file[i].cstr[1] == 'e' &&
-			file[i].cstr[2] == 't')
+		file[i] = make_ddString_length("", 0);
+	}
+
+	for (int i = 0; i < 22; i++)
+	{
+		comms[i] = make_ddString_length("", 0);
+	}
+
+
+	for(;;)
+	{
+		char* cinp = make(char, 50);
+		ddPrint_int(flen);
+		ddPrint_cstring(": ");
+		fgets(cinp,50,stdin);
+		ddString ds = make_ddString(cinp);
+		if (cinp[0] == 'r' &&
+			cinp[1] == 'u' &&
+			cinp[2] == 'n')
 		{
-			if (file[i].cstr[4] == 'i' &&
-				file[i].cstr[5] == 'n' &&
-				file[i].cstr[6] == 't')
+			compute();
+		}
+		else if (cinp[0] == 't' &&
+			cinp[1] == 'i' &&
+			cinp[2] == 'm' &&
+			cinp[3] == 'e')
+		{
+			ddTimer_start();
+			compute();
+			double ti = ddTimer_stop();
+			ddPrint_nl();
+			ddPrint_cstring("RunTime: ");
+			ddPrint_double_nl(ti);
+		}
+		else if (cinp[0] == 'c' &&
+			cinp[1] == 'l' &&
+			cinp[2] == 's')
+		{
+			cursor_clear();
+		}
+		else if (cinp[0] == 'l' &&
+			cinp[1] == 'i' &&
+			cinp[2] == 's' &&
+			cinp[3] == 't')
+		{
+			ddPrint_nl();
+			for (int i = 0; i < flen; i++)
 			{
-				int v = 5;
-				stack_add(&stack, make_ddString((char*)&(file[i].cstr[8])),
-						(void*)&v, dtint);
+				ddPrint_int(i);
+				ddPrint_cstring(": ");
+				ddPrint_ddString(file[i]);
 			}
 		}
-		if (file[i].cstr[0] == 'f' &&
-			file[i].cstr[1] == 'n')
+		else if (cinp[0] == 'c' &&
+			cinp[1] == 'l' &&
+			cinp[2] == 'e' &&
+			cinp[3] == 'a' &&
+			cinp[4] == 'r')
 		{
-			if (file[i].cstr[3] == 'p' &&
-				file[i].cstr[4] == 'r' &&
-				file[i].cstr[5] == 'i' &&
-				file[i].cstr[6] == 'n' &&
-				file[i].cstr[7] == 't')
-
+			for (int i = 0; i < 20; i++)
+			{
+				file[i] = make_ddString_length("", 0);
+			}
+			flen = 0;
+		}
+		else
+		{
+			file[flen] = ds;
+			flen++;
 		}
 	}
+
 
 
 
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc ddScript.c -o ddScript -lm
