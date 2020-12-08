027a7e60564f7650109099be85fd1260dd542a13
SmokeyTheKat <owr5273@gmail.com>
2020-12-07 23:39:12 -0400

    added file write

--- src/file.h
+++ src/file.h
@@ -1,6 +1,14 @@
 #ifndef __ddScript_file_h__
 #define __ddScript_file_h__
 
+void write_file(const char* path, ddString content)
+{
+	FILE* fp = fopen(path, "w");
+	if (fp == null) return;
+	fwrite(content.cstr, sizeof(char), content.length, fp);
+	fclose(fp);
+}
+
 ddString read_file(const char* path)
 {
 	FILE* fp = fopen(path, "r");
