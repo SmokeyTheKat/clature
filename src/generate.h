811b5e9e9254ebbce7fe89024f77b4b823bacef2
smokeythekat <owr5273@gmail.com>
2021-03-26 12:05:02 -0300

    clearn

--- src/generate.h
+++ src/generate.h
@@ -257,259 +257,47 @@ void generate_tree(struct tokenNode* node)
 	switch (type)
 	{
 		case NTYPE_OB:
-		{
-			scopeStack[scopeStackPos++] = stackt.top;
-			scope++;
-		} break;
+			gen_scope_up(node);
+			break;
 		case NTYPE_CB:
-		{
-			stackt.top = scopeStack[--scopeStackPos];
-			scope--;
-			btc_set(BTC_LABEL, make_format_ddString(".SC%d%d", scope, scopeCounts[scope]), REG_NONE);
-			scopeCounts[scope]++;
-		} break;
+			gen_scope_down(node);
+			break;
 		case NTYPE_ASM:
-		{
-			node->nodes[0]->value->value.cstr[0] = '	';
-			btc_set(BTC_ILA, node->nodes[0]->value->value, REG_NONE);
-		} break;
+			gen_asm(node);
+			break;
 		case NTYPE_VALUE:
-		{
-			if (ddString_is_number(node->value->value))
-				gen_push(node->value->value);
-			else
-			{
-				push_stack_var(*stackt_get_var(node->value->value));
-			}
-		} break;
+			gen_value(node);
+			break;
 		case NTYPE_DEREF:
-		{
-			gen_split(node, 1);
-			gen_pop(REG_R8);
-			push_ref(REG_R8, ddString_to_int(node->nodes[3]->value->value));
-		} break;
+			gen_deref(node);
+			break;
 		case NTYPE_VAR_DEF:
-		{
-			stackt_set_var(node->nodes[0]->value->value,
-					ddString_to_int(node->nodes[1]->value->value));
-		} break;
+			gen_var_def(node);
+			break;
 		case NTYPE_MOD:
-		{
-			switch (node->nodes[1]->value->type)
-			{
-				case TKN_QUEST:
-				{
-					struct stVariable* var = stackt_get_var(node->nodes[0]->value->value);
-					btc_set(BTC_MOV, REG_R8, REG_RBP);
-					btc_set(BTC_SUB, REG_R8, make_ddString_from_int(var->spos));
-					gen_push(REG_R8);
-				} break;
-			}
-		} break;
+			gen_mod(node);
+			break;
 		case NTYPE_SUB_CALL:
-		{
-			addSize = true;
-			sizet paramCount = (node->nodes[0]->nodeCount)/2;
-			for (sizet i = (paramCount*2)-1; i >= 0; i -= 2)
-			{
-				generate_tree(node->nodes[0]->nodes[i-1]);
-			}
-			addSize = false;
-			btc_set(BTC_CALL, node->nodes[1]->value->value, REG_NONE);
-			btc_set(BTC_ADD, REG_RSP, make_ddString_from_int(addSizeVal));
-			addSizeVal = 0;
-			if (statementIsEquality)
-				gen_push(REG_R8);
-		} break;
+			gen_sub_call(node);
+			break;
 		case NTYPE_RETURN:
-		{
-			gen_split(node, 0);
-			gen_pop(REG_R8);
-			btc_set(BTC_JMP, make_format_ddString(".SC%d%d", 0, scopeCounts[0]), REG_NONE);
-		} break;
+			gen_return(node);
+			break;
 		case NTYPE_SUB:
-		{
-			struct bitcode* bitcodeHead = bitcode_get_first();
-			struct bitcode* temp = btcptr;
-			struct bitcode* functionCodeHead = make(struct bitcode, 1);
-			btcptr = functionCodeHead;
-			struct bitcode* btcMoveStack = gen_sub_headder(node->nodes[1]->value->value);
-			fstackt.size = 0;
-			fstackt.top = 0;
-			switch_stacks();
-			sizet paramCount = (node->nodes[0]->nodeCount)/2;
-			struct tokenNode* pnode = node->nodes[0]->nodes[node->nodes[0]->nodeCount-1];
-			sizet tsize = 0;
-			for (sizet i = 0; i < (paramCount*2)-1; i += 2)
-			{
-				int csize = ddString_to_int(node->nodes[0]->nodes[i]->nodes[1]->value->value);
-				stackt_set_param_var(node->nodes[0]->nodes[i]->nodes[0]->value->value, csize, tsize);
-				tsize += csize;
-			}
-			sizet tscope = scope;
-			struct tokenNode* cnode = next_tree();
-			generate_tree(cnode);
-			while (scope != tscope)
-			{
-				cnode = next_tree();
-				generate_tree(cnode);
-			}
-			btc_set(BTC_ADD, REG_RSP, make_ddString_from_int(stackt.size));
-			gen_sub_footer();
-			btcMoveStack->r2 = make_ddString_from_int(stackt.size);
-			bitcodeHead->prev = btcptr->prev;
-			btcptr->prev->next = bitcodeHead;
-			bitcodeHead = functionCodeHead;
-			btcptr = temp;
-			switch_stacks();
-		} break;
+			gen_sub(node);
+			break;
 		case NTYPE_FOR:
-		{
-			sizet stotop = stackt.top;
-			gen_split(node->nodes[0], 4);
-			btc_set(BTC_LABEL, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]), REG_NONE);
-			sizet tscope = scope;
-			struct tokenNode* cnode = next_tree();
-			generate_tree(cnode);
-			while (scope != tscope)
-			{
-				cnode = next_tree();
-				generate_tree(cnode);
-			}
-			gen_split(node->nodes[0], 0);
-			statementIsEquality = true;
-			gen_split(node->nodes[0], 2);
-			gen_pop(REG_R8);
-			btc_set(BTC_CMP, REG_R8, make_constant_ddString("1"));
-			btc_set(BTC_JE, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]-1), REG_NONE);
-			statementIsEquality = false;
-			stackt.top = stotop;
-		} break;
+			gen_for(node);
+			break;
 		case NTYPE_WHILE:
-		{
-			btc_set(BTC_LABEL, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]), REG_NONE);
-			sizet tscope = scope;
-			struct tokenNode* cnode = next_tree();
-			generate_tree(cnode);
-			while (scope != tscope)
-			{
-				cnode = next_tree();
-				generate_tree(cnode);
-			}
-			statementIsEquality = true;
-			gen_split(node, 0);
-			gen_pop(REG_R8);
-			btc_set(BTC_CMP, REG_R8, make_constant_ddString("1"));
-			btc_set(BTC_JE, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]-1), REG_NONE);
-			statementIsEquality = false;
-		} break;
+			gen_while(node);
+			break;
 		case NTYPE_IF:
-		{
-			statementIsEquality = true;
-			gen_split(node, 0);
-			gen_pop(REG_R8);
-			btc_set(BTC_CMP, REG_R8, make_constant_ddString("0"));
-			btc_set(BTC_JE, make_format_ddString(".SC%d%d", scope, scopeCounts[scope]), REG_NONE);
-			bool iselse = false;
-			sizet tscope = scope;
-			statementIsEquality = false;
-			generate_tree(trees[treePos++]);
-			struct tokenNode* cnode;
-			while (scope != tscope)
-			{
-				cnode = next_tree();
-				if (cnode->nodeCount == 1 && cnode->nodes[0]->value->value.cstr[0] == '}')
-				{
-					struct tokenNode* nextTree = peek_tree();
-					if (nextTree && nextTree->nodeCount == 1 && nextTree->nodes[0]->value->symbol == G_KW_ELSE)
-					{
-						iselse = true;
-						btc_set(BTC_JMP, make_format_ddString(".SC%d%d", scope-1, scopeCounts[scope-1]+1), REG_NONE);
-					}
-				}
-				generate_tree(cnode);
-			}
-			if (iselse)
-			{
-				next_tree();
-				tscope = scope;
-				do
-				{
-					cnode = next_tree();
-					generate_tree(cnode);
-				} while (scope != tscope);
-			}
-		} break;
+			gen_if(node);
+			break;
 		case NTYPE_THO:
-		{
-			switch (node->nodes[1]->value->type)
-			{
-				case TKN_EQUALS:
-				{
-					statementIsEquality = true;
-					struct stVariable* var;
-					int n2class = node_classify(node->nodes[2]);
-					if (n2class == NTYPE_VAR_DEF)
-					{
-						var = stackt_set_var(node->nodes[2]->nodes[0]->value->value,
-							ddString_to_int(node->nodes[2]->nodes[1]->value->value));
-					}
-					else if (n2class == NTYPE_DEREF)
-					{
-						gen_split(node, 0);
-						gen_split(node->nodes[2], 1);
-						gen_pop(REG_R8);
-						pop_ref(REG_R8, ddString_to_int(node->nodes[2]->nodes[3]->value->value));
-						break;
-					}
-					else var = stackt_get_var(node->nodes[2]->value->value);
-
-					gen_split(node, 0);
-					pop_stack_var(*var);
-					statementIsEquality = false;
-				} break;
-				case TKN_EQUALS_EQUALS:
-					gen_cmp(node, compare_equal_flag);
-					break;
-				case TKN_NOT_EQUALS:
-					gen_cmp(node, compare_not_equal_flag);
-					break;
-				case TKN_LESS_THAN_EQUALS:
-					gen_cmp(node, compare_lessthan_equal_flag);
-					break;
-				case TKN_GREATER_THAN_EQUALS:
-					gen_cmp(node, compare_greaterthan_equal_flag);
-					break;
-				case TKN_LESS_THAN:
-					gen_cmp(node, compare_lessthan_flag);
-					break;
-				case TKN_GREATER_THAN:
-					gen_cmp(node, compare_greaterthan_flag);
-					break;
-				case TKN_MUL:
-				case TKN_DIV:
-				{
-					gen_split(node, 0);
-					gen_split(node, 2);
-					gen_pop(REG_RAX);
-					gen_pop(REG_R8);
-					int opc = toh_get_opc(node->nodes[1]);
-					btc_set(opc, REG_R8, REG_NONE);
-					gen_push(REG_RAX);
-				} break;
-				default:
-				{
-					gen_split(node, 0);
-					gen_split(node, 2);
-					gen_pop(REG_R8);
-					gen_pop(REG_R9);
-					int opc = toh_get_opc(node->nodes[1]);
-					btc_set(opc, REG_R8, REG_R9);
-					gen_push(REG_R8);
-				} break;
-			}
-
-		} break;
+			gen_tho(node);
+			break;
 	}
 }
 
@@ -566,6 +354,287 @@ struct bitcode* generate_bitcode_main(struct tokenNode** parseTrees, sizet _tree
 	btcptr = temp;
 
 	return bitcodeHead;
+}
+
+void gen_scope_up(struct tokenNode* node)
+{
+	scopeStack[scopeStackPos++] = stackt.top;
+	scope++;
+}
+void gen_scope_down(struct tokenNode* node)
+{
+	stackt.top = scopeStack[--scopeStackPos];
+	scope--;
+	btc_set(BTC_LABEL, make_format_ddString(".SC%d%d", scope, scopeCounts[scope]), REG_NONE);
+	scopeCounts[scope]++;
+}
+
+void gen_asm(struct tokenNode* node)
+{
+	node->nodes[0]->value->value.cstr[0] = '	';
+	btc_set(BTC_ILA, node->nodes[0]->value->value, REG_NONE);
+}
+
+void gen_value(struct tokenNode* node)
+{
+	if (ddString_is_number(node->value->value))
+		gen_push(node->value->value);
+	else
+	{
+		push_stack_var(*stackt_get_var(node->value->value));
+	}
+}
+
+void gen_deref(struct tokenNode* node)
+{
+	gen_split(node, 1);
+	gen_pop(REG_R8);
+	push_ref(REG_R8, ddString_to_int(node->nodes[3]->value->value));
+}
+
+void gen_var_def(struct tokenNode* node)
+{
+	stackt_set_var(node->nodes[0]->value->value,
+			ddString_to_int(node->nodes[1]->value->value));
+}
+
+void gen_sub_call(struct tokenNode* node)
+{
+	addSize = true;
+	sizet paramCount = (node->nodes[0]->nodeCount)/2;
+	for (sizet i = (paramCount*2)-1; i >= 0; i -= 2)
+	{
+		generate_tree(node->nodes[0]->nodes[i-1]);
+	}
+	addSize = false;
+	btc_set(BTC_CALL, node->nodes[1]->value->value, REG_NONE);
+	btc_set(BTC_ADD, REG_RSP, make_ddString_from_int(addSizeVal));
+	addSizeVal = 0;
+	if (statementIsEquality)
+		gen_push(REG_R8);
+}
+
+void gen_return(struct tokenNode* node)
+{
+	gen_split(node, 0);
+	gen_pop(REG_R8);
+	btc_set(BTC_JMP, make_format_ddString(".SC%d%d", 0, scopeCounts[0]), REG_NONE);
+}
+
+void gen_sub(struct tokenNode* node)
+{
+	struct bitcode* bitcodeHead = bitcode_get_first();
+	struct bitcode* temp = btcptr;
+	struct bitcode* functionCodeHead = make(struct bitcode, 1);
+	btcptr = functionCodeHead;
+	struct bitcode* btcMoveStack = gen_sub_headder(node->nodes[1]->value->value);
+	fstackt.size = 0;
+	fstackt.top = 0;
+	switch_stacks();
+	sizet paramCount = (node->nodes[0]->nodeCount)/2;
+	struct tokenNode* pnode = node->nodes[0]->nodes[node->nodes[0]->nodeCount-1];
+	sizet tsize = 0;
+	for (sizet i = 0; i < (paramCount*2)-1; i += 2)
+	{
+		int csize = ddString_to_int(node->nodes[0]->nodes[i]->nodes[1]->value->value);
+		stackt_set_param_var(node->nodes[0]->nodes[i]->nodes[0]->value->value, csize, tsize);
+		tsize += csize;
+	}
+	sizet tscope = scope;
+	struct tokenNode* cnode = next_tree();
+	generate_tree(cnode);
+	while (scope != tscope)
+	{
+		cnode = next_tree();
+		generate_tree(cnode);
+	}
+	btc_set(BTC_ADD, REG_RSP, make_ddString_from_int(stackt.size));
+	gen_sub_footer();
+	btcMoveStack->r2 = make_ddString_from_int(stackt.size);
+	bitcodeHead->prev = btcptr->prev;
+	btcptr->prev->next = bitcodeHead;
+	bitcodeHead = functionCodeHead;
+	btcptr = temp;
+	switch_stacks();
+}
+
+void gen_for(struct tokenNode* node)
+{
+	sizet stotop = stackt.top;
+	gen_split(node->nodes[0], 4);
+	btc_set(BTC_LABEL, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]), REG_NONE);
+	sizet tscope = scope;
+	struct tokenNode* cnode = next_tree();
+	generate_tree(cnode);
+	while (scope != tscope)
+	{
+		cnode = next_tree();
+		generate_tree(cnode);
+	}
+	gen_split(node->nodes[0], 0);
+	statementIsEquality = true;
+	gen_split(node->nodes[0], 2);
+	gen_pop(REG_R8);
+	btc_set(BTC_CMP, REG_R8, make_constant_ddString("1"));
+	btc_set(BTC_JE, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]-1), REG_NONE);
+	statementIsEquality = false;
+	stackt.top = stotop;
+}
+
+void gen_while(struct tokenNode* node)
+{
+	btc_set(BTC_LABEL, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]), REG_NONE);
+	sizet tscope = scope;
+	struct tokenNode* cnode = next_tree();
+	generate_tree(cnode);
+	while (scope != tscope)
+	{
+		cnode = next_tree();
+		generate_tree(cnode);
+	}
+	statementIsEquality = true;
+	gen_split(node, 0);
+	gen_pop(REG_R8);
+	btc_set(BTC_CMP, REG_R8, make_constant_ddString("1"));
+	btc_set(BTC_JE, make_format_ddString(".WL%d%d", scope, scopeCounts[scope]-1), REG_NONE);
+	statementIsEquality = false;
+}
+
+void gen_if(struct tokenNode* node)
+{
+	statementIsEquality = true;
+	gen_split(node, 0);
+	gen_pop(REG_R8);
+	btc_set(BTC_CMP, REG_R8, make_constant_ddString("0"));
+	btc_set(BTC_JE, make_format_ddString(".SC%d%d", scope, scopeCounts[scope]), REG_NONE);
+	bool iselse = false;
+	sizet tscope = scope;
+	statementIsEquality = false;
+	generate_tree(trees[treePos++]);
+	struct tokenNode* cnode;
+	while (scope != tscope)
+	{
+		cnode = next_tree();
+		if (cnode->nodeCount == 1 && cnode->nodes[0]->value->value.cstr[0] == '}')
+		{
+			struct tokenNode* nextTree = peek_tree();
+			if (nextTree && nextTree->nodeCount == 1 && nextTree->nodes[0]->value->symbol == G_KW_ELSE)
+			{
+				iselse = true;
+				btc_set(BTC_JMP, make_format_ddString(".SC%d%d", scope-1, scopeCounts[scope-1]+1), REG_NONE);
+			}
+		}
+		generate_tree(cnode);
+	}
+	if (iselse)
+	{
+		next_tree();
+		tscope = scope;
+		do
+		{
+			cnode = next_tree();
+			generate_tree(cnode);
+		} while (scope != tscope);
+	}
+}
+
+void gen_toh(struct tokenNode* node)
+{
+	gen_split(node, 0);
+	gen_split(node, 2);
+	gen_pop(REG_R8);
+	gen_pop(REG_R9);
+	int opc = toh_get_opc(node->nodes[1]);
+	btc_set(opc, REG_R8, REG_R9);
+	gen_push(REG_R8);
+}
+void gen_rax_toh(struct tokenNode* node)
+{
+	gen_split(node, 0);
+	gen_split(node, 2);
+	gen_pop(REG_RAX);
+	gen_pop(REG_R8);
+	int opc = toh_get_opc(node->nodes[1]);
+	btc_set(opc, REG_R8, REG_NONE);
+	gen_push(REG_RAX);
+}
+void gen_equals(struct tokenNode* node)
+{
+	statementIsEquality = true;
+	struct stVariable* var;
+	int n2class = node_classify(node->nodes[2]);
+	if (n2class == NTYPE_VAR_DEF)
+	{
+		var = stackt_set_var(node->nodes[2]->nodes[0]->value->value,
+			ddString_to_int(node->nodes[2]->nodes[1]->value->value));
+	}
+	else if (n2class == NTYPE_DEREF)
+	{
+		gen_split(node, 0);
+		gen_split(node->nodes[2], 1);
+		gen_pop(REG_R8);
+		pop_ref(REG_R8, ddString_to_int(node->nodes[2]->nodes[3]->value->value));
+		return;
+	}
+	else var = stackt_get_var(node->nodes[2]->value->value);
+
+	gen_split(node, 0);
+	pop_stack_var(*var);
+	statementIsEquality = false;
+}
+
+void gen_mod_operator(struct tokenNode* node)
+{
+	struct stVariable* var = stackt_get_var(node->nodes[0]->value->value);
+	btc_set(BTC_MOV, REG_R8, REG_RBP);
+	btc_set(BTC_SUB, REG_R8, make_ddString_from_int(var->spos));
+	gen_push(REG_R8);
+}
+
+void gen_mod(struct tokenNode* node)
+{
+	switch (node->nodes[1]->value->type)
+	{
+		case TKN_QUEST:
+			gen_mod_operator(node);
+			break;
+	}
+}
+
+
+void gen_tho(struct tokenNode* node)
+{
+	switch (node->nodes[1]->value->type)
+	{
+		case TKN_EQUALS:
+			gen_equals(node);
+			break;
+		case TKN_EQUALS_EQUALS:
+			gen_cmp(node, compare_equal_flag);
+			break;
+		case TKN_NOT_EQUALS:
+			gen_cmp(node, compare_not_equal_flag);
+			break;
+		case TKN_LESS_THAN_EQUALS:
+			gen_cmp(node, compare_lessthan_equal_flag);
+			break;
+		case TKN_GREATER_THAN_EQUALS:
+			gen_cmp(node, compare_greaterthan_equal_flag);
+			break;
+		case TKN_LESS_THAN:
+			gen_cmp(node, compare_lessthan_flag);
+			break;
+		case TKN_GREATER_THAN:
+			gen_cmp(node, compare_greaterthan_flag);
+			break;
+		case TKN_MUL:
+		case TKN_DIV:
+			gen_rax_toh(node);
+			break;
+		default:
+			gen_toh(node);
+			break;
+	}
 }
 
 
